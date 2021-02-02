ec2997a5ac45128bd0d5085f7dfeabb74ee6269e
Josiah Stewart <>
2021-02-02 13:38:17 -0600

    Made it possible to choose VR mode in the inspector

--- Assets/Scripts/CameraMovement.gd
+++ Assets/Scripts/CameraMovement.gd
@@ -4,13 +4,17 @@ var speed = 2
 var mouse_sens = 0.3
 var camera_anglev=0
 
-func _ready():
-	print("Hello World!")
-	var interfaces = ARVRServer.get_interfaces()
-	for i in interfaces.size():
-		print (interfaces[i])
-		
-	var VR = ARVRServer.find_interface("Native mobile")
+enum VR_MODE { NONE, NATIVE_MOBILE, OPEN_VR }
+
+export(VR_MODE) var vrMode = VR_MODE.NONE
+
+func _ready():	
+	
+	if vrMode == VR_MODE.NONE:
+		print("No VR")
+		return
+	
+	var VR = ARVRServer.find_interface(getVRModeString(vrMode))
 	if VR and VR.initialize():
 		get_viewport().arvr = true
 		get_viewport().hdr = false
@@ -18,7 +22,8 @@ func _ready():
 		OS.vsync_enabled = false
 		Engine.target_fps = 90
 	else:
-		print("No VR")
+		printerr("Unable to use selected VR interface, falling back to No VR")		
+	return
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
@@ -38,3 +43,10 @@ func _input(event):
 		if camera_anglev+changev>-50 and camera_anglev+changev<50:
 			camera_anglev+=changev
 			$ARVRCamera.rotate_x(deg2rad(changev))
+			
+func getVRModeString(vrMode):
+	if vrMode == VR_MODE.OPEN_VR:
+		return "OpenVR"
+	elif vrMode == VR_MODE.NATIVE_MOBILE:
+		return "Native mobile"	
+		

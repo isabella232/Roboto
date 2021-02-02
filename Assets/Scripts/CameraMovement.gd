787f4a1212599d566a198ffbf3445671e9b46fa0
Josiah Stewart <>
2021-02-02 14:29:34 -0600

    Added a new car model, added ability to move up and down in game

--- Assets/Scripts/CameraMovement.gd
+++ Assets/Scripts/CameraMovement.gd
@@ -1,11 +1,12 @@
 extends Spatial
 
-var speed = 2
-var mouse_sens = 0.3
-var camera_anglev=0
-
 enum VR_MODE { NONE, NATIVE_MOBILE, OPEN_VR }
 
+var camera_anglev = 0
+
+export(float) var speed = 2
+export(float) var speedBoost = 2
+export(float) var mouse_sens = 0.3
 export(VR_MODE) var vrMode = VR_MODE.NONE
 
 func _ready():	
@@ -26,14 +27,21 @@ func _ready():
 	return
 
 func _physics_process(delta):
+	var actualSpeed = speed 
+	if Input.is_action_pressed("speed_boost"):
+		actualSpeed += speedBoost
 	if Input.is_action_pressed("move_right"):
-		translate_object_local($ARVRCamera.transform.basis.x * speed * delta)
+		translate_object_local($ARVRCamera.transform.basis.x * actualSpeed * delta)
 	if Input.is_action_pressed("move_left"):
-		translate_object_local(-$ARVRCamera.transform.basis.x * speed * delta)
+		translate_object_local(-$ARVRCamera.transform.basis.x * actualSpeed * delta)
 	if Input.is_action_pressed("move_forward"):
-		translate_object_local(-$ARVRCamera.transform.basis.z * speed * delta)
+		translate_object_local(-$ARVRCamera.transform.basis.z * actualSpeed * delta)
 	if Input.is_action_pressed("move_backward"):
-		translate_object_local($ARVRCamera.transform.basis.z * speed * delta)
+		translate_object_local($ARVRCamera.transform.basis.z * actualSpeed * delta)
+	if Input.is_action_pressed("move_up"):
+		translate_object_local(-$ARVRCamera.transform.basis.y * actualSpeed * delta)
+	if Input.is_action_pressed("move_down"):
+		translate_object_local($ARVRCamera.transform.basis.y * actualSpeed * delta)
 	pass
 
 func _input(event):
